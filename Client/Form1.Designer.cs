58453026f5754d4dd112c534e25a5c697b897d03
NoahNuoXu <73081410+NoahNuoXu@users.noreply.github.com>
2022-02-28 09:44:51 -0800

    Add files via upload

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace _2222222
+{
+    partial class frmClient
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 
+
+        private void InitializeComponent()
+        {
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxDistance = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.z1 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.z2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.BackColor = System.Drawing.Color.White;
+            this.txtMsg.Location = new System.Drawing.Point(288, 266);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(263, 96);
+            this.txtMsg.TabIndex = 9;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(441, 75);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(110, 38);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("SimSun", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPort.Location = new System.Drawing.Point(481, 21);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(70, 29);
+            this.txtPort.TabIndex = 7;
+            this.txtPort.Text = "8088";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(418, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIp.Location = new System.Drawing.Point(136, 22);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(252, 29);
+            this.txtIp.TabIndex = 6;
+            this.txtIp.Text = "192.168.1.14";
+            this.txtIp.TextChanged += new System.EventHandler(this.txtIp_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Server:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendMsg.Location = new System.Drawing.Point(437, 164);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(114, 46);
+            this.btnSendMsg.TabIndex = 10;
+            this.btnSendMsg.Text = "Send";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(216, 145);
+            this.x1.MaxLength = 11;
+            this.x1.Multiline = true;
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(55, 20);
+            this.x1.TabIndex = 11;
+            this.x1.TextChanged += new System.EventHandler(this.x1_TextChanged);
+            this.x1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.x1_KeyPress);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisconnect.Location = new System.Drawing.Point(307, 75);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(116, 38);
+            this.btnDisconnect.TabIndex = 18;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 144);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 24);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "First 3D Point: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(43, 206);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 24);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Second 3D Point: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 266);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 24);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Distance: ";
+            // 
+            // textBoxDistance
+            // 
+            this.textBoxDistance.Font = new System.Drawing.Font("SimSun", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxDistance.Location = new System.Drawing.Point(146, 267);
+            this.textBoxDistance.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxDistance.Name = "textBoxDistance";
+            this.textBoxDistance.Size = new System.Drawing.Size(125, 29);
+            this.textBoxDistance.TabIndex = 23;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(288, 145);
+            this.y1.MaxLength = 11;
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(55, 20);
+            this.y1.TabIndex = 24;
+            this.y1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.y1_KeyPress);
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(363, 145);
+            this.z1.MaxLength = 11;
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(55, 20);
+            this.z1.TabIndex = 25;
+            this.z1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.z1_KeyPress);
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(288, 207);
+            this.y2.MaxLength = 11;
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(55, 20);
+            this.y2.TabIndex = 26;
+            this.y2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.y2_KeyPress);
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(216, 207);
+            this.x2.MaxLength = 11;
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(55, 20);
+            this.x2.TabIndex = 27;
+            this.x2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.x2_KeyPress);
+            // 
+            // z2
+            // 
+            this.z2.Location = new System.Drawing.Point(363, 207);
+            this.z2.MaxLength = 11;
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(55, 20);
+            this.z2.TabIndex = 28;
+            this.z2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.z2_KeyPress);
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 411);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.textBoxDistance);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(708, 505);
+            this.MinimumSize = new System.Drawing.Size(621, 450);
+            this.Name = "frmClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxDistance;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox z1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox z2;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace _2222222
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Thread threadClient = null;
+        Socket sockClient = null;
+        string recClientIpAndPort = "";
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse(txtIp.Text.Trim());
+            IPEndPoint endPoint=new IPEndPoint (ip,int.Parse(txtPort.Text.Trim()));
+            sockClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                ShowMsg("Connecting to server...");
+                sockClient.Connect(endPoint);
+                
+            }
+            catch (SocketException skep)
+            {
+                MessageBox.Show(skep.Message);
+                return;
+                //this.Close();
+            }
+            ShowMsg("Connected successfully!");
+            threadClient = new Thread(RecMsg);
+            threadClient.IsBackground = true;
+            threadClient.Start();
+
+        }
+
+        void RecMsg()
+        {
+            while (true)
+            {
+                byte[] arrMsgRec = new byte[1024 * 1024 * 2];
+                int length = -1;
+                try
+                {
+                    length = sockClient.Receive(arrMsgRec);
+                }
+                catch (SocketException errorOccurred)
+                {
+                    ShowMsg("Error；" + errorOccurred.Message);
+                    return;
+                }
+                catch (Exception errorOccurred)
+                {
+                    ShowMsg("Error："+errorOccurred.Message);
+                    return;
+                }
+
+                if (arrMsgRec[0] == 0)
+                {
+                    string msgRecStr = System.Text.Encoding.UTF8.GetString(arrMsgRec, 1, length - 1);
+                    ShowMsg(msgRecStr);
+                }
+
+
+                if (arrMsgRec[0] == 1)
+                {
+                    string msgRecStr = System.Text.Encoding.UTF8.GetString(arrMsgRec, 1, length - 1);
+                    textBoxDistance.Text = msgRecStr;
+                }
+
+                if (arrMsgRec[0] == 2)
+                {
+                    string msgRecStr = System.Text.Encoding.UTF8.GetString(arrMsgRec, 1, length - 1);
+                    ShowMsg(msgRecStr);
+                    recClientIpAndPort = msgRecStr;
+                }
+
+
+            }
+        }
+        void ShowMsg(string str)
+        {
+            txtMsg.AppendText(str + "\r\n");
+        }
+
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            string point1 = x1.Text.Trim() + "," +  y1.Text.Trim() + "," + z1.Text.Trim();
+            string point2 = x2.Text.Trim() + "," + y2.Text.Trim() + "," + z2.Text.Trim();
+            string strMsg = "The first point is : " + point1 + "\r\n" 
+                            + "The second point is : " +  point2;
+            string sendMsg = point1 + ";" + point2 + ";" + recClientIpAndPort;
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+            byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+            arrSendMsg[0] = 0;
+            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+            sockClient.Send(arrSendMsg);
+          
+            ShowMsg(strMsg);
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        private void txtIp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void x1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void x1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        
+
+        private void y1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void z1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void x2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void y2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void z2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && e.KeyChar != '.')
+            {
+                if ((e.KeyChar < '0') || (e.KeyChar > '9'))
+                {
+                    e.Handled = true;
+                }
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
