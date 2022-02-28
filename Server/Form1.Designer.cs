a7c89d4e7a11b8e593464dd66c565bebad31ba9c
NoahNuoXu <73081410+NoahNuoXu@users.noreply.github.com>
2022-02-28 09:44:22 -0800

    Add files via upload

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace _11111
+{
+    partial class frm_server
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region
+
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnBeginListen = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMsgSend = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbOnline = new System.Windows.Forms.ListBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIp.Location = new System.Drawing.Point(91, 32);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(369, 29);
+            this.txtIp.TabIndex = 1;
+            this.txtIp.Text = "192.168.1.14";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(469, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(532, 32);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(79, 29);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "8088";
+            // 
+            // btnBeginListen
+            // 
+            this.btnBeginListen.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeginListen.Location = new System.Drawing.Point(481, 289);
+            this.btnBeginListen.Name = "btnBeginListen";
+            this.btnBeginListen.Size = new System.Drawing.Size(130, 46);
+            this.btnBeginListen.TabIndex = 2;
+            this.btnBeginListen.Text = "Start Server";
+            this.btnBeginListen.UseVisualStyleBackColor = true;
+            this.btnBeginListen.Click += new System.EventHandler(this.btnBeginListen_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.BackColor = System.Drawing.Color.White;
+            this.txtMsg.Location = new System.Drawing.Point(12, 81);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(451, 191);
+            this.txtMsg.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(481, 393);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(130, 46);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMsgSend
+            // 
+            this.txtMsgSend.Location = new System.Drawing.Point(7, 292);
+            this.txtMsgSend.Multiline = true;
+            this.txtMsgSend.Name = "txtMsgSend";
+            this.txtMsgSend.Size = new System.Drawing.Size(455, 143);
+            this.txtMsgSend.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(469, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IP List:";
+            // 
+            // lbOnline
+            // 
+            this.lbOnline.FormattingEnabled = true;
+            this.lbOnline.Location = new System.Drawing.Point(481, 99);
+            this.lbOnline.Name = "lbOnline";
+            this.lbOnline.Size = new System.Drawing.Size(130, 173);
+            this.lbOnline.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(481, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Close Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frm_server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 462);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbOnline);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMsgSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnBeginListen);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(703, 653);
+            this.MinimumSize = new System.Drawing.Size(616, 486);
+            this.Name = "frm_server";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion 
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnBeginListen;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMsgSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbOnline;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;  // For IP，IPAddress, IPEndPoint，Port
+using System.Threading;
+using System.IO;
+
+namespace _11111
+{
+    public partial class frm_server : Form
+    {
+        public frm_server()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        // clients' connections
+        Thread threadWatch = null; 
+        Socket socketWatch = null;
+
+        // saving new Socket and Thread
+        Dictionary<string, Socket> dict = new Dictionary<string, Socket>();
+        Dictionary<string, Thread> dictThread = new Dictionary<string, Thread>();
+
+        string toSendClinet = "";
+        private void btnBeginListen_Click(object sender, EventArgs e)
+        {
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Parse(txtIp.Text.Trim());  // getting IP address
+                // creating object for ip and port
+                IPEndPoint endPoint = new IPEndPoint(address, int.Parse(txtPort.Text.Trim()));
+                try
+                {
+                    // bind ip and port
+                    socketWatch.Bind(endPoint);
+                }
+                catch (SocketException errorOccurred)
+                {
+                    MessageBox.Show("Error："+errorOccurred.Message);
+                    return;
+                }
+                // set clents up to 6 for the requirement
+                socketWatch.Listen(6);
+                // threads
+                threadWatch = new Thread(WatchConnecting);
+                threadWatch.IsBackground = true;
+                threadWatch.Start();
+                ShowMsg("Server has started successfully！");
+        }
+
+        void WatchConnecting()
+        {
+            while (true)
+            {
+                // Accept() method is used to obtain the socket which a client's connection is detected.
+                Socket sokConnection = socketWatch.Accept(); 
+                // adding client's IP into the list box
+                lbOnline.Items.Add(sokConnection.RemoteEndPoint.ToString());
+                // adding socket contents connected to client's profile
+                dict.Add(sokConnection.RemoteEndPoint.ToString(), sokConnection);
+                ShowMsg("Client connected successfully！");
+                // sending the client's ip and port back to the client
+                // due to the last problem of always sending the result for last client's app
+                string sendMsg = sokConnection.RemoteEndPoint.ToString();
+                // convert into byte
+                byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+                // creating new byte
+                byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+                // sending result
+                arrSendMsg[0] = 2;
+                Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+                ShowMsg("Replying the client: " + sendMsg);
+                
+                Thread thr = new Thread(RecMsg);
+                thr.IsBackground = true;
+                thr.Start(sokConnection);
+                // adding new threads
+                dictThread.Add(sokConnection.RemoteEndPoint.ToString(), thr);
+                sokConnection.Send(arrSendMsg);
+            }
+        }
+
+        void RecMsg(object sokConnectionparn)
+        {
+                Socket sokClient = sokConnectionparn as Socket;
+                while (true)
+                {
+                    // define a 2M cache because got overload last time and the program is the bottleneck
+                    byte[] contentRec = new byte[1024 * 1024 * 2];
+                    // save content into the variable
+                    int length = -1;
+                    try
+                    {
+                        length = sokClient.Receive(contentRec);  // the real length of receiving data
+                    }
+                    catch (SocketException errorHappened)
+                    {
+                        ShowMsg("Error：" + errorHappened.Message);
+                        // remove everything
+                        dict.Remove(sokClient.RemoteEndPoint.ToString());
+                        dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                        lbOnline.Items.Remove(sokClient.RemoteEndPoint.ToString());
+                        break;
+                    }
+                // if data received
+                if (contentRec[0] == 0)
+                {
+                    if (contentRec[1] == 0 && contentRec[2] == 0 && contentRec[3] == 0 && contentRec[4] == 0) 
+                    {
+                        return;
+                    }
+                    //convert to string
+                    string msgRecStr = System.Text.Encoding.UTF8.GetString(contentRec, 1, length - 1);
+
+                    ShowMsg("The Two 3D points: " + msgRecStr);
+
+                    // sending
+                    string sendMsg = getCalculte(msgRecStr).ToString();
+                    // convert into byte
+                    byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+                    // creating new byte
+                    byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+                    // sending result
+                    arrSendMsg[0] = 1; 
+                    Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+                    // solving error by adding this, need search more
+                    dict[toSendClinet].Send(arrSendMsg);
+
+                    ShowMsg("Distance: " + sendMsg);
+                }
+
+
+            }     
+        }
+        // for calculating distance
+        double getCalculte(string str)
+        {
+            int numericValue;
+            // get the two points
+            string[] rows = str.Split(new char[] { ';' });
+            string[] strPoint1 = rows[0].Split(new char[] { ',' });
+            string[] strPoint2 = rows[1].Split(new char[] { ',' });
+            // getting the specific client's ip and port number
+            toSendClinet = rows[2];
+            double[] point1 = { 0, 0, 0 };
+            double[] point2 = { 0, 0, 0 };
+
+            // the first point
+            point1[0] = double.Parse(strPoint1[0]);
+            point1[1] = double.Parse(strPoint1[1]);
+            point1[2] = double.Parse(strPoint1[2]);
+            // the second point
+            point2[0] = double.Parse(strPoint2[0]);
+            point2[1] = double.Parse(strPoint2[1]);
+            point2[2] = double.Parse(strPoint2[2]);
+            // the formula
+            double value = Math.Pow(Math.Abs(point1[0] - point2[0]), 2.0)
+                + Math.Pow(Math.Abs(point1[1] - point2[1]), 2.0)
+                + Math.Pow(Math.Abs(point1[2] - point2[2]), 2.0);
+
+            return Math.Round(Math.Sqrt(value), 2);
+        }
+        void ShowMsg(string str)
+        {
+            txtMsg.AppendText(str + "\r\n");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msgRecStr = "Server" + "\r\n" + "   -->" + txtMsgSend.Text.Trim() + "\r\n";
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(msgRecStr); 
+            byte[] arrSendMsg=new byte[arrMsg.Length+1];
+            arrSendMsg[0] = 0; 
+            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+            string clientInList = "";
+            clientInList = lbOnline.Text.Trim();
+            if (string.IsNullOrEmpty(clientInList))   // if none of clients selected, will show warning
+            {
+                MessageBox.Show("Please select the client.");
+            }
+            else
+            {
+                dict[clientInList].Send(arrSendMsg);
+                ShowMsg(msgRecStr);
+                txtMsgSend.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            socketWatch.Close();
+            threadWatch.Abort();
+            this.Dispose();
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
